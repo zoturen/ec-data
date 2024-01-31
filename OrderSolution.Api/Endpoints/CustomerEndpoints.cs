a640f860736219ffaaa23db536afdd61303bc0fc
Chrille Jansson <chrillejansson@me.com>
2024-01-31 16:14:43 +0100

    Customers:
    Added endpoints

--- OrderSolution.Api/Endpoints/CustomerEndpoints.cs
+++ OrderSolution.Api/Endpoints/CustomerEndpoints.cs
@@ -7,7 +7,10 @@ public static class CustomerEndpoints
 {
     public static void MapCustomerEndpoints(this IEndpointRouteBuilder endpoints)
     {
-        endpoints.MapPost("/", async (CustomerCreateDto customerCreateDto, ICustomerService customerService) =>
+        var customerEndpoint = endpoints.MapGroup("/customers")
+            .WithTags("Customers API");
+        
+        customerEndpoint.MapPost("/", async (CustomerCreateDto customerCreateDto, ICustomerService customerService) =>
         {
             var customer = await customerService.CreateAsync(customerCreateDto);
             if (customer == null!)
@@ -16,13 +19,13 @@ public static class CustomerEndpoints
             return Results.Created($"/{customer.Id}", customer);
         });
         
-        endpoints.MapGet("/", async (ICustomerService customerService) =>
+        customerEndpoint.MapGet("/", async (ICustomerService customerService) =>
         {
             var customers = await customerService.GetAllAsync();
             return Results.Ok(customers);
         });
         
-        endpoints.MapGet("/{id:guid}", async (Guid id, ICustomerService customerService) =>
+        customerEndpoint.MapGet("/{id:guid}", async (Guid id, ICustomerService customerService) =>
         {
             var customer = await customerService.GetAsync(id);
             if (customer == null!)
@@ -31,7 +34,7 @@ public static class CustomerEndpoints
             return Results.Ok(customer);
         });
         
-        endpoints.MapPut("/{id:guid}", async (Guid id, CustomerCreateDto customerUpdateDto, ICustomerService customerService) =>
+        customerEndpoint.MapPut("/{id:guid}", async (Guid id, CustomerCreateDto customerUpdateDto, ICustomerService customerService) =>
         {
             var result = await customerService.UpdateAsync(id, customerUpdateDto);
             if (result)
@@ -40,7 +43,7 @@ public static class CustomerEndpoints
             return Results.Ok();
         });
         
-        endpoints.MapDelete("/{id:guid}", async (Guid id, ICustomerService customerService) =>
+        customerEndpoint.MapDelete("/{id:guid}", async (Guid id, ICustomerService customerService) =>
         {
             var result = await customerService.DeleteAsync(id);
             if (result)
