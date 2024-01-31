dec3c17bfc46acf66a3a44617ec721dfce061434
Chrille Jansson <chrillejansson@me.com>
2024-01-31 16:14:29 +0100

    Categories:
    Added endpoints

--- OrderSolution.Api/Endpoints/CategoryEndpoints.cs
+++ OrderSolution.Api/Endpoints/CategoryEndpoints.cs
@@ -7,28 +7,31 @@ public static class CategoryEndpoints
 {
     public static void AddCategoryEndpoints(this IEndpointRouteBuilder endpoints)
     {
-        endpoints.MapGet("/", async (ICategoryService categoryService) =>
+        var categoryEndpoints = endpoints.MapGroup("/categories")
+            .WithTags("Categories API");
+        
+        categoryEndpoints.MapGet("/", async (ICategoryService categoryService) =>
             Results.Ok(await categoryService.GetCategoriesAsync()));
 
-        endpoints.MapGet("/{id:guid}", async (Guid id, ICategoryService categoryService) =>
+        categoryEndpoints.MapGet("/{id:guid}", async (Guid id, ICategoryService categoryService) =>
         {
             var category = await categoryService.GetCategoryAsync(id);
             return category is null ? Results.NotFound() : Results.Ok(category);
         });
         
-        endpoints.MapPost("/", async (CategoryCreateDto dto, ICategoryService categoryService) =>
+        categoryEndpoints.MapPost("/", async (CategoryCreateDto dto, ICategoryService categoryService) =>
         {
             var category = await categoryService.CreateCategoryAsync(dto);
             return Results.Created($"/{category.Id}", category);
         });
         
-        endpoints.MapPut("/{id:guid}", async (Guid id, CategoryCreateDto dto, ICategoryService categoryService) =>
+        categoryEndpoints.MapPut("/{id:guid}", async (Guid id, CategoryCreateDto dto, ICategoryService categoryService) =>
         {
             var result = await categoryService.UpdateCategoryAsync(id, dto);
             return result ? Results.Ok() : Results.NotFound();
         });
         
-        endpoints.MapDelete("/{id:guid}", async (Guid id, ICategoryService categoryService) =>
+        categoryEndpoints.MapDelete("/{id:guid}", async (Guid id, ICategoryService categoryService) =>
         {
             var result = await categoryService.DeleteCategoryAsync(id);
             return result ? Results.Ok() : Results.NotFound();
